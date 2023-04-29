b5341604a835d2cfb1d9bf3b67a928d57af2a468
Cody Burns <cody.w.burns@gmail.com>
2023-04-29 12:35:26 -0500

    Create snowpack.config.js

---
+++ snowpack.config.js
@@ -0,0 +1,27 @@
+// Snowpack Configuration File 
+ // See all supported options: https://www.snowpack.dev/reference/configuration 
+  
+ /** @type {import("snowpack").SnowpackUserConfig } */ 
+ module.exports = { 
+   mount: { 
+     public: { url: "/", static: true }, 
+     src: { url: "/dist" }, 
+   }, 
+   plugins: ["@prefresh/snowpack"], 
+   optimize: { 
+     minify: true, 
+   }, 
+   packageOptions: { 
+     /* ... */ 
+   }, 
+   devOptions: { 
+     /* ... */ 
+   }, 
+   buildOptions: { 
+     /* ... */ 
+   }, 
+   alias: { 
+     react: "preact/compat", 
+     "react-dom": "preact/compat", 
+   }, 
+ };
