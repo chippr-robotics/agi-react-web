f6cfee68c41b2328682e1dfe3bd2e46c3c36d973
Burns, Cody <cody.burns@accenture.com>
2023-08-02 10:43:36 -0500

    clean up

--- src/index.jsx
+++ src/index.jsx
@@ -17,39 +17,10 @@ export const ContextCounter = createContext(null);
 
 
 function App() {
-   const [playSound, setPlaySound] = useState(false); 
-   const [boxColor, setBoxColor] = useState("red"); 
-   const colors = ["red","blue", "green", "orange"];
-   function handleClick(){
-      let newColor = (colors.indexOf(boxColor) + 1) % colors.length
-      setBoxColor(colors[newColor]);
-      let audio = document.querySelector("#whale");
-      audio.click();   
-   }
-   let counter ={};
    return (
       <Scene >
          <ContextCounter.Provider value={counter}>
           <Environment />
-          <Entity 
-            primitive="a-box" 
-            position="1 1 1"
-            color= {boxColor}
-            class="raycasterable" 
-            events = {{
-               click: handleClick
-            }}
-            animation__click="startEvents: click; property: position; to: 30 0 0; dir:alternate; loop: false; dur: 10000" >
-            <Entity 
-            id="whale"
-            primitive="a-sound"
-            src= "url(./dist/assets/sound/whale.mp3)" 
-            autoplay= "false" 
-            loop="false"
-            on="click"
-            />
-          </Entity>
-            
           <User />
          </ContextCounter.Provider>
       </Scene> 
